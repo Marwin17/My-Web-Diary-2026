b7a6c01c3404db581fff842922cc28c0b5b2c572
Marwin Tan <tan.marwin17@gmail.com>
2026-04-15 10:00:15 +0800

    Added filter and sorting to diary list and TinyMCE editor for DiaryAddEdit.tsx

--- src/screens/DiaryItems.tsx
+++ src/screens/DiaryItems.tsx
@@ -1,19 +1,24 @@
-import { Fab } from "@mui/material";
-import DiaryList from "../diary/DiaryList"
+import Fab from "@mui/material/Fab"
 import AddIcon from '@mui/icons-material/Add';
+
+import DiaryList from "../diary/DiaryList"
+import { useNavigate } from "react-router";
 
 function DiaryItems() {
 
+    const navigate = useNavigate()
+
     return (
         <>
-       <DiaryList/>
-       <Fab color="secondary" aria-label="add" sx={{ 
-        position: 'fixed',
-        right: '16px',
-       }}>
-            <AddIcon />
-          </Fab>
-       </>
+            <DiaryList />
+            <Fab color="secondary" aria-label="add" sx={{
+                position: 'fixed',
+                right: '16px',
+                bottom: '16px'
+            }} onClick={() => navigate('/diaryedit')}>
+                <AddIcon />
+            </Fab>
+        </>
     )
 }
 
